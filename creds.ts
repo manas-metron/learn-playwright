e90abf10398b9b2fc001b619bf50d449ace62e03
manas <manas@metronlabs.com>
2025-01-23 09:58:46 +0530

    feat : credentials variable.

---
+++ creds.ts
@@ -0,0 +1,10 @@
+export const creds = {
+    "USERNAME": "mshinde@paloaltonetworks.com",
+    "PASSWORD": "<password>",
+    "TOTP": "<code>",
+    "AWSAccessKeyId": "<KeyId>",
+    "AWSSecretAccessKey": "<accessKey>",
+    "DIGloginUrl": "https://app.dig.security//",
+    "bucketName": "automation-test-444",
+    "REGION": "us-west-1"
+}
